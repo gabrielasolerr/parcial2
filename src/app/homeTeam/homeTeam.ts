8b69dd0e86456b67a141ab684362620c5511a1df
gabrielasolerr <g.soler@uniandes.edu.co>
2023-05-19 07:49:29 -0500

    Cambios iniciales

---
+++ src/app/homeTeam/homeTeam.ts
@@ -0,0 +1,18 @@
+export class HomeTeam {
+  country: string;
+  goals: number;
+  name: string;
+  penalties: number
+
+  constructor(
+    country: string,
+    goals: number,
+    name: string,
+    penalties: number
+  ) {
+    this.country = country;
+    this.goals = goals;
+    this.name = name;
+    this.penalties = penalties
+  }
+}
